7a360c236718a91120a108f96db3b9080caa1558
Jan Fiedler <janfiedler@gmail.com>
2022-09-17 00:52:27 +0200

    New config example

--- config.example.js
+++ config.example.js
@@ -3,24 +3,14 @@ const { TOKEN, INVARIANT_FEE_TIERS, dataTemplate } = require('./tools.js');
 // Settings for LP's
 const LPs = [
     {
-        fromInvariant: true, // If true, first buy tokenY on invariant and then sell for tokenX on jupiter. False is the opposite.
         bothAssets: true, // If you have funded your wallet with both assets, bot can skip waiting for node sync after first swap
         tokenX: TOKEN.USDC,
         tokenY: TOKEN.MSOL,
-        tokenAmount: 0.22,
+        tokenAmount: 0.2,
         minUnitProfit: 400,
-        invariantFee: INVARIANT_FEE_TIERS[1],
-        data: {...dataTemplate}
-    },
-    {
-        fromInvariant: false,
-        bothAssets: true,
-        tokenX: TOKEN.USDC,
-        tokenY: TOKEN.MSOL,
-        tokenAmount: 0.22,
-        minUnitProfit: 400,
-        invariantFee: INVARIANT_FEE_TIERS[1],
-        data: {...dataTemplate}
+        invariantFee: INVARIANT_FEE_TIERS[2],
+        dataInvJup: {...dataTemplate},
+        dataJupInv: {...dataTemplate}
     }
 ];
 
